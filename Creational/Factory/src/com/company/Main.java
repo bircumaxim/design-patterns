cfd5ad985c29fcc3ec9826e08e217d4619ec91e5
Bircu Maxim <maxim.bircu@jivygorup.com>
2016-11-15 17:01:53 +0200

    Created factory module.

---
+++ Creational/Factory/src/com/company/Main.java
@@ -0,0 +1,10 @@
+package com.company;
+
+/**
+ * Created by bircumaxim on 11/15/2016.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
