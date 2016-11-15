410c20e7c2589b3ca106351e5dae0d864410b341
Bircu Maxim <maxim.bircu@jivygorup.com>
2016-11-15 20:43:28 +0200

    Implemented prototype pattern example.

--- Creational/Prototype/src/com/company/Main.java
+++ Creational/Prototype/src/com/company/Main.java
@@ -1,10 +1,19 @@
 package com.company;
 
+import com.company.Shapes.Circle;
+
 /**
  * Created by bircumaxim on 11/15/2016.
  */
 public class Main {
     public static void main(String[] args) {
-        //TODO implement here prototype pattern example.
+        CloneFactory shapeMaker = new CloneFactory();
+
+        Circle circle = new Circle("Circle1");
+
+        Circle circle2 = (Circle) shapeMaker.getClone(circle);
+
+        circle.render();
+        circle2.render();
     }
 }
